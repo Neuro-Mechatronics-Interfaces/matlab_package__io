3d9a11017c4502f0bcb847e3436d3139686184b2
Max-MI115 <max.murphy11@gmail.com>
2022-09-27 15:44:19 -0400

    Modify format for load_percept_data

--- load_percept_data.m
+++ load_percept_data.m
@@ -26,6 +26,7 @@ if numel(BLOCK) > 1
     for ii = 1:numel(BLOCK)
         sens_data{ii} = io.load_percept_data(SUBJ, YYYY, MM, DD, BLOCK(ii), varargin{:});
     end
+    sens_data = vertcat(sens_data{:});
     return;
 end
 
@@ -36,6 +37,8 @@ gui_str = sprintf('%s_RadioCheckSlider_%d.yaml', tank, BLOCK);
 img_str = sprintf('%s_Sensation*_%d.png', tank, BLOCK);
 
 sens_data = struct;
+sens_data.tank = tank;
+sens_data.block = BLOCK;
 sens_data.fname.pixel = fullfile(pars.raw_data_folder, SUBJ, tank, 'notes', tank, pixel_str);
 sens_data.fname.gui = fullfile(pars.raw_data_folder, SUBJ, tank, 'notes', tank, gui_str);
 sens_data.fname.img = fullfile(pars.raw_data_folder, SUBJ, tank, 'notes', tank, img_str);
@@ -57,12 +60,17 @@ else
     s = textscan(fid, '%s', 'Delimiter', '\n');
     fclose(fid);
     idx = [find(startsWith(s{1}, 'Sensation')); numel(s{1})+1];
+    sens_data.gui = cell(numel(sens_data.fname.gui), 1);
     for ii = 1:(numel(idx)-1)
         vec = (idx(ii)+1):(idx(ii+1)-1);
-        sens_data.gui.(sprintf('Sensation%d', ii-1)) = io.yaml.load(strjoin(s{1}(vec), '\n')); 
+        sens_data.gui{ii, 1} = io.yaml.load(strjoin(s{1}(vec), '\n')); 
     end
 end
 
 sens_data.img = dir(sens_data.fname.img);
-
+if isempty(sens_data.img)
+    sens_data.imread = @()[];
+else
+    sens_data.imread = @()imread(fullfile(sens_data.img(end).folder, sens_data.img(end).name));
+end
 end
