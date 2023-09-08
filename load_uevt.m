36c97180818a7463275404705c4207103412db64
Max-MI115 <max.murphy11@gmail.com>
2023-09-08 14:04:47 -0400

    Adds "translation" output returned as 3rd output from load_uevt
    For making it easier to interpret the logs when reading in table format.

--- load_uevt.m
+++ load_uevt.m
@@ -1,9 +1,9 @@
-function [T,fname] = load_uevt(options)
+function [T,fname,E] = load_uevt(options)
 %LOAD_UEVT  Load .uevt file (which contains wrist task behavior events)
 %
 % Syntax:
 %   T = io.load_uevt('Name', value, ...);
-%   [T,fname] = ...
+%   [T,fname,E] = ...
 %
 % Example 1:
 %   T = io.load_uevt('File',"R:/path/to/data/Subject_2023_07_26_MID.uevt");
@@ -17,18 +17,41 @@ function [T,fname] = load_uevt(options)
 %
 % Inputs:
 %   options:
+%     AddTranslation (1,1) logical = true % Set true to include 'Translation' column in timetables of third returned output
 %     File {mustBeTextScalar} = 'none' % Can be full filename of file to load
 %     CommentCharacter {mustBeTextScalar} = '#';
 %     StartingRow (1,1) double = 4;
+%     EnumeratedEvents (:,2) string = ["State", "TaskState"; 
+%                                      "Target", "TaskTarget"; 
+%                                      "Outcome", "TaskOutcome"; 
+%                                      "Direction", "TaskDirection"; 
+%                                      "Target", "TaskTarget"; 
+%                                      "Orientation", "TaskOrientation"];
+%     UTCOffset (1,1) double = -4
+%     Subject {mustBeTextScalar} = '';
+%     Year (1,1) double = year(today);
+%     Month (1,1) double = month(today);
+%     Day (1,1) double = day(today);
+%     Orientation {mustBeTextScalar, mustBeMember(options.Orientation, {'MID', 'PRO'})} = 'MID';
 %
 % Output:
 %   T - Timetable with columns 'Time' | 'Event' | 'Data' --> related to
 %           Jetson wrist task logs.
 %   fname - Name of the opened file.
+%   E - Cell array of enumerated timetables with "translated" data. See
+%       options.EnumeratedEvents for which events will be enumerated. The
+%       first column are Event names and corresponding element in second
+%       column is the enumeration class (in +enum) used to decode Data.
+%       Each cell element will be a timetable where the first column is
+%       Time and second column is a variable named by the matched element
+%       from the first column of options.EnumeratedEvents (e.g. the first
+%       table in default options will contain 'Time' | 'State' as the two
+%       columns).
 %
 % See also: Contents
 
 arguments
+    options.AddTranslation (1,1) logical = true;
     options.File {mustBeTextScalar} = 'none'
     options.CommentCharacter {mustBeTextScalar} = '#';
     options.DataShareRoot {mustBeFolder} = "R:\NMLShare\raw_data\primate";
@@ -41,6 +64,7 @@ arguments
     options.Month (1,1) double = month(today);
     options.Day (1,1) double = day(today);
     options.Orientation {mustBeTextScalar, mustBeMember(options.Orientation, {'MID', 'PRO'})} = 'MID';
+    options.EnumeratedEvents (:,2) string = ["State", "TaskState"; "Target", "TaskTarget"; "Outcome", "TaskOutcome"; "Direction", "TaskDirection"; "Target", "TaskTarget"; "Orientation", "TaskOrientation"];
 end
 
 if strcmpi(options.File, 'none')
@@ -122,4 +146,25 @@ T.Time = datetime(T.Time,'ConvertFrom','posixtime', ...
 T.Time = T.Time - hours(options.UTCOffset);
 T = table2timetable(T,'RowTimes','Time');
 T.Properties.UserData = meta;
+
+if nargout > 2
+    E = cell(size(options.EnumeratedEvents,1),2);
+    for ii = 1:size(options.EnumeratedEvents,1)
+        E{ii,1} = char(options.EnumeratedEvents(ii,1));
+        idx = strcmpi(T.Event, E{ii,1});
+        Time = T.Time(idx);
+        tmp1 = enum.(options.EnumeratedEvents(ii,2))(T.Data(idx));
+        if options.AddTranslation
+            Translation = string(tmp1);
+            tmp2 = table(Time, tmp1, Translation);
+        else
+            tmp2 = table(Time, tmp1);
+        end
+        tmp2.Properties.VariableNames{2} = E{ii,1};
+        E{ii,2} = table2timetable(tmp2);
+    end
+else
+    E = {};
+end
+
 end
